3dd4de60f4338aff69ca955201513b6085086151
naderkhaled15 <https://github.com/naderkhaled15>
2024-10-28 16:45:05 +0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="splash-view">
+    <div class="nav-btns row">
+      <div class="col-12 col-sm-6 icon-box">
+        <div
+          class="w-100 d-flex justify-content-center align-items-end"
+          style="height: 60%"
+        >
+          <img
+            src="/src/assets/splash/Component 12.png"
+            style="width: 18.9rem; height: auto"
+            alt="mount wolf developer"
+          />
+        </div>
+        <button class="see-btn h-auto">
+          <a
+            style="display: inline-block"
+            target="_blank"
+            href="sw.mount-wolf.com"
+          >
+            Visit Website
+          </a>
+          <svg
+            class="mx-2"
+            style="width: 0.8rem; height: 0.8rem; font-weight: bold"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5.73975 3.58334L3.40641 1.25001L4.00016 0.666678L7.3335 4.00001L4.00016 7.33334L3.40641 6.75001L5.73975 4.41668L0.666829 4.41668L0.666829 3.58334L5.73975 3.58334Z"
+              fill="#FEF7FF"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <div
+        class="vr m-0 p-0"
+        style="width: 1px; height: 100%; background-color: #ffffff !important"
+      ></div>
+
+      <div class="col-12 col-sm-6 icon-box">
+        <div
+          class="w-100 d-flex justify-content-center align-items-end"
+          style="height: 60%"
+        >
+          <img
+            class="pb-3"
+            style="width: 27.8rem; height: auto"
+            src="/src/assets/splash/Component 13.png"
+            alt="mount wolf marketing"
+          />
+        </div>
+        <button class="see-btn h-auto">
+          <a
+            style="display: inline-block"
+            target="_blank"
+            href="dig.mount-wolf.com"
+          >
+            Visit Website
+          </a>
+          <svg
+            class="mx-2"
+            style="width: 0.8rem; height: 0.8rem; font-weight: bold"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5.73975 3.58334L3.40641 1.25001L4.00016 0.666678L7.3335 4.00001L4.00016 7.33334L3.40641 6.75001L5.73975 4.41668L0.666829 4.41668L0.666829 3.58334L5.73975 3.58334Z"
+              fill="#FEF7FF"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
